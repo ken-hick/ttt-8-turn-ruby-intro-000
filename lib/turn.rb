2b0517472c323e66412bf0631f5b6f95a5dbe6b4
Ken Hick <hick.ken@gmail.com>
2018-10-30 14:17:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,20 @@ end
 def input_to_index(input)
   input.to_i - 1
 end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" or board[index] == "O"
+    true
+  end
+end
