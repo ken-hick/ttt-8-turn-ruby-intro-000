529a1e603690957d649b26d0ecf9b8d47ec22765
Ken Hick <hick.ken@gmail.com>
2018-10-30 14:14:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(index)
-  index = user_input - 1
+def input_to_index(input)
+  input to.i - 1
 end
